dd17b1d8c2d4075cfbf7dc8877f6d3578926aed4
BigTandy <Buddyjack264@GMAIL.COM>
2021-11-20 21:00:49 -0500

    Add files via upload

---
+++ db.py
@@ -0,0 +1,35 @@
+import mysql.connector
+import asyncio
+
+class dataBase:
+
+    def __init__(self) -> None:
+        pass
+
+    
+    def select(self, query, values) -> list:
+        db = mysql.connector.connect(user='', password='', host='127.0.0.1', database='messages')
+        cursor = db.cursor(dictionary=True, buffered=True)
+
+        cursor.execute(query, values) # %s for item to be replaced in querry
+
+        rows = cursor.fetchall()
+
+        #db.commit()
+        cursor.close()
+        db.close()
+
+        return rows
+
+
+    def insert(self, query, values):
+        db = mysql.connector.connect(user='', password='', host='127.0.0.1', database='messages')
+        cursor = db.cursor()
+
+        cursor.execute(query, values) # %s for item to be replaced in querry
+
+        db.commit()
+        cursor.close()
+        db.close()
+
+    

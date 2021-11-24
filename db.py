fc143ddd5f4be5ccf965636e3fac2971b70e2009
BigTandy <Buddyjack264@GMAIL.COM>
2021-11-23 21:12:25 -0500

    Update db.py

--- db.py
+++ db.py
@@ -8,7 +8,7 @@ class dataBase:
 
     
     def select(self, query, values) -> list:
-        db = mysql.connector.connect(user='python', password='Cheese', host='127.0.0.1', database='messages')
+        db = mysql.connector.connect(user='python', password='', host='127.0.0.1', database='messages')
         cursor = db.cursor(dictionary=True, buffered=True)
 
         cursor.execute(query, values) # %s for item to be replaced in querry
@@ -23,7 +23,7 @@ class dataBase:
 
 
     def insert(self, query, values):
-        db = mysql.connector.connect(user='python', password='Cheese', host='127.0.0.1', database='messages')
+        db = mysql.connector.connect(user='python', password='', host='127.0.0.1', database='messages')
         cursor = db.cursor()
 
         cursor.execute(query, values) # %s for item to be replaced in querry
